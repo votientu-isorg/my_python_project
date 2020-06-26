9086c7d18b1d32549dda1da7e03e1bc1edb9a956
votientu <votientu@gmail.com>
2020-06-26 10:09:24 +0200

    Add greeting and participant count

--- greeting.py
+++ greeting.py
@@ -2,3 +2,4 @@ __author__ = "Tien Tu Vo"
 
 print('Bienvenue à la formation Git')
 print("Je m'appelle Tien Tu, je suis participant dans le cours Git")
+print('Vous pensez que nous sommes 3 mais je vous dis que nous sommes 4')
